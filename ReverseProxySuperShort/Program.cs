48a64f8fbb35d73ab554911a1b52950d5d1ed4c2
Jose A <jose.fernandez@ceiba.com.co>
2021-04-10 10:18:38 -0500

    Added top level statment version - super short

---
+++ ReverseProxySuperShort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+await Host.CreateDefaultBuilder().ConfigureWebHostDefaults(web => {
+    web.Configure(builder => {
+        builder.UseRouting();
+        builder.UseEndpoints(endpoints => {
+            endpoints.MapReverseProxy();
+        });
+    }).ConfigureServices((WebHostBuilder, services) => {
+        var config = services.BuildServiceProvider().GetService<IConfiguration>();
+        services.AddReverseProxy()
+            .LoadFromConfig(config.GetSection("ReverseProxy"));
+    });
+}).Build().RunAsync();
